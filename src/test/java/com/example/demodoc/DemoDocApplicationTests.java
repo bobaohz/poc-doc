4a156006a4a1f6ef9baa18fabbb9c15609a6b815
BOB-T14P\super <starlightbao@sina.com>
2024-01-13 22:44:04 +0800

    first COMMIT

---
+++ src/test/java/com/example/demodoc/DemoDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
